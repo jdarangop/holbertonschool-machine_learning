32323e6bdc1f714f00f43045b2f0d051273a274a
jdarangop <juandiegoap33@gmail.com>
2020-03-29 07:21:42 +0000

    Add task 2 and 3

---
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Class Neuron """
+import numpy as np
+
+
+class Neuron(object):
+    """Neuron"""
+    def __init__(self, nx):
+        """ Init method """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        else:
+            if nx < 1:
+                raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ Getter W """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter b """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter A """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Method for Forward Propagation """
+        self.__A = 1.0/(1.0 + np.exp(-(np.dot(self.W, X) + self.b)))
+        return self.__A
---
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Class Neuron """
+import numpy as np
+
+
+class Neuron(object):
+    """Neuron"""
+    def __init__(self, nx):
+        """ Init method """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        else:
+            if nx < 1:
+                raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ Getter W """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter b """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter A """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Method for Forward Propagation """
+        self.__A = 1.0/(1.0 + np.exp(-(np.dot(self.W, X) + self.b)))
+        return self.__A
+
+    def cost(self, Y, A):
+        """ Method to compute the cost """
+        cost_log = np.dot(A, np.log(Y.T + 1.0000001))
+        cost_log += np.dot((1.0000001 - A), np.log(1.0000001 - Y.T))
+        cost_log *= (-1/Y.shape[1])
+        return cost_log[0][0]
