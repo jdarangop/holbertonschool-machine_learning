a858cebf1d1d486071b2e163b40b90ed8c1044e4
jdarangop <juandiegoap33@gmail.com>
2020-04-15 16:42:55 +0000

    Add task 15 slice like a ninja

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Slice Like A Ninja """
+
+
+def np_slice(matrix, axes={}):
+    """ slices a matrxi along a specific axes
+        matrix: numpy ndarray
+        axes: dict
+    """
+    list_slices = []
+    for i in range(matrix.ndim):
+        tupla = axes.get(i)
+        list_slices.append(slice(None) if tupla is None else slice(*tupla))
+    return matrix[list_slices]
