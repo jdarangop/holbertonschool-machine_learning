9c611b5a31e1090357b8456c1fff8a39b99ed73b
jdarangop <juandiegoap33@gmail.com>
2020-04-21 04:10:09 +0000

    Add task 8 PCA

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -8,7 +8,7 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-plt.scatter(x, y, s=z, c=z, cmap='viridis')
+plt.scatter(x, y, c=z, cmap='viridis')
 colorbar = plt.colorbar()
 colorbar.set_label('elevation (m)')
 
