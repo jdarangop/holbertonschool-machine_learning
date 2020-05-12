4b3882e0c25e70d8cc1814851cc00f740349f8db
jdarangop <juandiegoap33@gmail.com>
2020-05-12 22:54:41 +0000

    Try to fix outputs task 0

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -9,8 +9,8 @@ def one_hot_encode(Y, classes):
         classes: is the maximum number of classes found in Y
         Return: numpy ndarray with one-hot matrix.
     """
-    # if any(map(lambda i: i < 0, Y)) or Y is None or classes < 0:
-    #     return None
+    if any(map(lambda i: i < 0, Y)) or Y is None or classes < 0:
+         return None
     try:
         result = np.zeros((classes, len(Y)))
         counter = 0
