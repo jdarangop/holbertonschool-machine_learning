c70b73a506edcacfb165b91619bfdebeafe8526d
jdarangop <juandiegoap33@gmail.com>
2020-05-12 23:30:12 +0000

    Fix output in task 1

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -8,10 +8,13 @@ def one_hot_decode(one_hot):
         one_hot: numpy ndarray with one hot.
         Return: numpy ndarray with categorical values
     """
-    if one_hot is None:
+    if type(one_hot) != np.ndarray:
         return None
     result = []
     for i in range(one_hot.shape[0]):
-        result.append(np.argmax(one_hot[:, i]))
+        element = np.argmax(one_hot[:, i])
+        if one_hot[element, i] != 1:
+            return None
+        result.append(element)
     result = np.array(result)
     return result
