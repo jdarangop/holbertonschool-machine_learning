fcb5e47114300ce6554165c8bc7c25a2eba4f62a
jdarangop <juandiegoap33@gmail.com>
2020-05-16 23:52:31 +0000

    Delete numpy importation

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ Train """
 import tensorflow as tf
-import numpy as np
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
