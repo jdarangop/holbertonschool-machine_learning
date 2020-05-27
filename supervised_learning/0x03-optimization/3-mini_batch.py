dcb4e9bd56945edf1021626ee54364dfb67dd540
jdarangop <juandiegoap33@gmail.com>
2020-05-27 20:57:36 +0000

    Fix task 3

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -32,8 +32,8 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
         for i in range(epochs + 1):
             accu_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
             loss_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            accu_valid = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            loss_valid = sess.run(loss, feed_dict={x: X_train, y: Y_train})
+            accu_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+            loss_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
             print("After {} epochs:".format(i))
             print("\tTraining Cost: {}".format(loss_train))
             print("\tTraining Accuracy: {}".format(accu_train))
