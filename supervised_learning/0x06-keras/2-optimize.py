0a35d1c0ee00b27e2aeda6924be14a673e9c2c8a
jdarangop <juandiegoap33@gmail.com>
2020-06-02 07:45:14 +0000

    Fix task 2

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -14,7 +14,7 @@ def optimize_model(network, alpha, beta1, beta2):
         Returns:
             None.
     """
-    Adam = K.optimizers.Adam(beta_1=beta1, beta_2=beta2)
+    Adam = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
     network.compile(optimizer=Adam,
                     loss="categorical_crossentropy",
                     metrics=['accuracy'])
