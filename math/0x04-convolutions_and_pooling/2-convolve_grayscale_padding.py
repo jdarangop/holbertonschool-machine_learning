97fd686387ea34dd7b6def80f1d025f736f281dd
jdarangop <juandiegoap33@gmail.com>
2020-06-05 05:21:26 +0000

    Fix task 2

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -3,60 +3,39 @@
 import numpy as np
 
 
-def convolution(image, kernel):
-    """ perform the convolution for an image.
-        Args:
-            image: (numpy.ndarray) image which the
-                   convolution should be applied.
-            kernel: (numpy.ndarray) kernel to perform
-                    the convolution.
-        Returns:
-            (numpy.ndarray) with the result of the convolution
-                            in the image.
-    """
-    img_row = image.shape[0]
-    img_col = image.shape[1]
-    kernel_side = kernel.shape[0]
-    # col_k = kernel.shape[1]
-    result = np.zeros((img_row - kernel_side + 1, img_col - kernel_side + 1))
-    i = 0
-    j = 0
-    while(True):
-        result[j][i] += np.sum(image[j:j + kernel_side,
-                               i:i + kernel_side] * kernel)
-        if i < img_col - kernel_side:
-            i += 1
-        elif j < img_row - kernel_side:
-            j += 1
-            i = 0
-        else:
-            return result
-
-
-def pad_with(vector, pad_width, iaxis, kwargs):
-    """pad_with function from numpy.pad documentation"""
-    pad_value = kwargs.get('padder', 0)
-    vector[:pad_width[0]] = pad_value
-    vector[-pad_width[1]:] = pad_value
-    return vector
-
-
 def convolve_grayscale_padding(images, kernel, padding):
-    """ performs a valid convolution on grayscale images keeping dim.
+    """ performs a valid convolution on grayscale images.
         Args:
             images: (numpy.ndarray) containing multiple grayscale images.
             kernel: (numpy.ndarray) containing the kernel for the convolution.
         Returns:
             (numpy.ndarray) containing the convoluded images.
     """
-    images_convoluded = []
-    for img in images:
-        # padding = int((kernel.shape[0] - 1) / 2)
-        img_pad = np.zeros((img.shape[0] + (2 * padding[0]),
-                            img.shape[1] + (2 * padding[1])))
-        img_pad[padding[0]:padding[0] + img.shape[0],
-                padding[1]:padding[1] + img.shape[1]] = img
-        # cover = np.zeros(i.shape[0] + (2 * padding))
-        # img_pad = np.pad(img, padding, pad_with)
-        images_convoluded.append(convolution(img_pad, kernel))
-    return np.array(images_convoluded)
+    number_img = images.shape[0]
+    img_row = images.shape[1]
+    img_col = images.shape[2]
+    kernel_row = kernel.shape[0]
+    kernel_col = kernel.shape[1]
+    i_row = img_row - kernel_row + 1
+    i_col = img_col - kernel_col + 1
+    # pad_row = max(int((kernel_row - 1) / 2), int(kernel_row / 2))
+    # pad_col = max(int((kernel_col - 1) / 2), int(kernel_col / 2))
+    pad_row = padding[0]
+    pad_col = padding[1]
+    images_pad = np.pad(images, ((0, 0), (pad_row, pad_row),
+                                 (pad_col, pad_col)),
+                        'constant', constant_values=(0))
+    result = np.zeros((number_img, images_pad.shape[1] - kernel_row + 1,
+                       images_pad.shape[2] - kernel_col + 1))
+    i = 0
+    j = 0
+    while(True):
+        result[:, j, i] = np.sum(images_pad[:, j:j + kernel_row,
+                                 i:i + kernel_col] * kernel, axis=(1, 2))
+        if i < img_col - kernel_col:
+            i += 1
+        elif j < img_row - kernel_row:
+            j += 1
+            i = 0
+        else:
+            return result
