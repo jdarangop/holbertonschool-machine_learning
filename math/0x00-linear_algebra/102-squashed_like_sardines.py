161e7e7d92c57e526eb974e21076a37834448c55
jdarangop <juandiegoap33@gmail.com>
2020-06-08 01:44:00 +0000

    Fix one check output wrong in task 102

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -13,11 +13,21 @@ def dim(matrix):
         return 1 + dim(matrix[0])
 
 
-def cat_matrices(mat1, mat2, axis=0):
-    """ concatenate two matrices
-        mat1: list
-        mat2: list
-        axis: int
+def matrix_shape(matrix):
+    """ find the shape of a matrix
+        matrix: list
+    """
+    dim = []
+    var = matrix.copy()
+    while(type(var) == list):
+        dim.append(len(var))
+        var = var[0]
+
+    return dim
+
+
+def concatenate(mat1, mat2, axis=0):
+    """ concatenate process
     """
 
     if dim(mat1) != dim(mat2) or axis > dim(mat1) or axis > dim(mat1):
@@ -34,3 +44,27 @@ def cat_matrices(mat1, mat2, axis=0):
                 return None
             else:
                 return result
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """ concatenate two matrices.
+        Args:
+            mat1: (list) matrix one.
+            mat2: (list) matrix two.
+            axis: (int) axis to concatenate.
+        Returns:
+            (list) with the matrix concatenate.
+            (None) if it's no posible.
+    """
+    array1 = matrix_shape(mat1)
+    array2 = matrix_shape(mat2)
+    if axis < len(array1) and axis < len(array2):
+        array1.pop(axis)
+        array2.pop(axis)
+    else:
+        return None
+
+    if array1 != array2:
+        return None
+    else:
+        return concatenate(mat1, mat2, axis)
