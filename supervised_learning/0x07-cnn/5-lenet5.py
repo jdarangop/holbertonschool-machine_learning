b55e7a7255c844273e1721b91216e60b43a9df37
jdarangop <juandiegoap33@gmail.com>
2020-06-12 19:02:06 +0000

    Avoid the use of tf in task 5

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ LeNet-5 (Keras) """
-import tensorflow as tf
 import tensorflow.keras as K
 
 
@@ -14,33 +13,31 @@ def lenet5(x):
             -(K.Model) compiled to use Adam optimization.
             -accuracy
     """
-    he_normal = tf.contrib.layers.variance_scaling_initializer()
-    relu = tf.nn.relu
     model = K.Sequential()
     model.add(K.layers.Conv2D(filters=6,
                               kernel_size=(5, 5),
                               padding='same',
-                              activation=relu,
-                              kernel_initializer=he_normal))
+                              activation='relu',
+                              kernel_initializer='he_normal'))
     model.add(K.layers.MaxPooling2D(pool_size=(2, 2),
               strides=(2, 2)))
     model.add(K.layers.Conv2D(filters=16,
                               kernel_size=(5, 5),
                               padding='valid',
-                              activation=relu,
-                              kernel_initializer=he_normal))
+                              activation='relu',
+                              kernel_initializer='he_normal'))
     model.add(K.layers.MaxPooling2D(pool_size=(2, 2),
                                     strides=(2, 2)))
     model.add(K.layers.Flatten(input_shape=(28, 28)))
     model.add(K.layers.Dense(units=120,
-                             activation=relu,
-                             kernel_initializer=he_normal))
+                             activation='relu',
+                             kernel_initializer='he_normal'))
     model.add(K.layers.Dense(units=84,
-                             activation=relu,
-                             kernel_initializer=he_normal))
+                             activation='relu',
+                             kernel_initializer='he_normal'))
     model.add(K.layers.Dense(units=10,
                              activation='softmax',
-                             kernel_initializer=he_normal))
+                             kernel_initializer='he_normal'))
     model.compile(optimizer='adam',
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
