df8407a806b8746b530fdf7691fc3e636223e194
jdarangop <juandiegoap33@gmail.com>
2020-06-18 21:56:49 +0000

    Add the folder 0x08-deep_cnns

---
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Inception Block """
+import tensorflow.keras as K
+
+
+def inception_block(A_prev, filters):
+    """ build an inception block
+        Args:
+            A_prev: (tf.Tensor) previous layer.
+            filters: (tuple) containing the number of filters.
+        Returns:
+            the concatenated output of the inception block.
+    """
+    F1, F3R, F3, F5R, F5, FPP = filters
+    conv1 = K.layers.Conv2D(F1, (1, 1), padding='same',
+                            activation='relu')(A_prev)
+    conv3R = K.layers.Conv2D(F3R, (1, 1), padding='same',
+                             activation='relu')(A_prev)
+    conv3 = K.layers.Conv2D(F3,  (3, 3), padding='same',
+                            activation='relu')(conv3R)
+    conv5R = K.layers.Conv2D(F5R, (1, 1), padding='same',
+                             activation='relu')(A_prev)
+    conv5 = K.layers.Conv2D(F5, (5, 5), padding='same',
+                            activation='relu')(conv5R)
+    maxpool = K.layers.MaxPooling2D((3, 3), strides=(1, 1),
+                                    padding='same')(A_prev)
+    convmax = K.layers.Conv2D(FPP, (1, 1), padding='same',
+                              activation='relu')(maxpool)
+
+    incep_block = K.layers.concatenate([conv1, conv3,
+                                        conv5, convmax])
+    return incep_block
---
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -0,0 +1,43 @@
+#/usr/bin/env python3
+""" Inception Network """
+import tensorflow.keras as K
+inception_block = __import__('0-inception_block').inception_block
+
+
+def inception_network():
+    """ builds the inception network.
+        Args:
+            Void.
+        Returns:
+            the keras model.
+    """
+    input_lay = K.Input(shape=(224, 224, 3))
+    layer = K.layers.Conv2D(64, (7, 7), padding='same', strides=(2, 2),
+                            activation='relu')(input_lay)
+    layer = K.layers.MaxPool2D((3, 3), padding='same',
+                               strides=(2, 2))(layer)
+    layer = K.layers.Conv2D(192, (3, 3), padding='same', strides=(1, 1),
+                            activation='relu')(layer)
+    layer = K.layers.MaxPool2D((3, 3), padding='same',
+                               strides=(2, 2))(layer)
+    layer = inception_block(layer, (64, 96, 128, 16, 32, 32))
+    layer = inception_block(layer, (128, 128, 192, 32, 96, 64))
+    layer = K.layers.MaxPool2D((3, 3), padding='same',
+                               strides=(2, 2))(layer)
+    layer = inception_block(layer, (192, 92, 208, 16, 48, 64))
+    layer = inception_block(layer, (160, 112, 224, 24, 64, 64))
+    layer = inception_block(layer, (128, 128, 256, 24, 64, 64))
+    layer = inception_block(layer, (112, 144, 288, 32, 64, 64))
+    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = K.layers.MaxPool2D((3, 3), padding='same',
+                               strides=(2, 2))(layer)
+    layer = inception_block(layer, (256, 160, 320, 32, 128, 128))
+    layer = inception_block(layer, (384, 192, 384, 48, 128, 128))
+    layer = K.layers.AveragePooling2D((7, 7), padding='same',
+                                       strides=(1, 1))(layer)
+    layer = K.layers.Dropout(0.4)(layer)
+    output = K.layers.Dense(units=1000, activation='softmax')(layer)
+
+    output = K.models.Model(inputs=input_lay, outputs=output)
+
+    return output
