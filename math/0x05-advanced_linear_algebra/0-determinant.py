c1d58977a22260ae2ef0c2ea89a634bd70d2b1cf
jdarangop <juandiegoap33@gmail.com>
2020-07-23 21:12:10 -0500

    Add the folder 0x05-advanced_linear_algebra

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Determinant """
+
+
+def recursion_determinant(mat):
+    """ operate the recursion in the determinant.
+        Args:
+            mat: (list/int) temporal matrix to calculate.
+        Returns:
+            (list/int) the result.
+    """
+    if len(mat) == 1:
+        return mat[0][0]
+    else:
+        result = 0
+        for i in range(len(mat)):
+            tmp = [j.copy() for j in mat]
+            indexs = tmp.pop(0)
+            for k in tmp:
+                k.pop(i)
+            result += ((-1) ** i) * indexs[i] * recursion_determinant(tmp)
+
+        return result
+
+
+def determinant(matrix):
+    """ calculate the determinant of a matrix.
+        Args:
+            matrix: (list)  list of lists which containg a matrix.
+        Returns:
+            (float) the determinant of the matrix.
+    """
+    if type(matrix) != list or matrix == [] or type(matrix[0]) != list:
+        raise TypeError('matrix must be a list of lists')
+    if matrix == [[]]:
+        return 1
+    num_rows = len(matrix)
+    for i in matrix:
+        if len(i) != num_rows:
+            raise ValueError('matrix must be a square matrix')
+    return recursion_determinant(matrix)
---
+++ math/0x05-advanced_linear_algebra/README.md
@@ -0,0 +1,13 @@
+# 0x05. Advanced Linear Algebra
+
+- 0. Determinant
+
+- 1. Minor
+
+- 2. Cofactor
+
+- 3. Adjugate
+
+- 4. Inverse
+
+- 5. Definiteness
