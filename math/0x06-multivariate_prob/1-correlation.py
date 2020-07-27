b4d84cf40a4b220ecb0bf3ca8b25765f83244099
jdarangop <juandiegoap33@gmail.com>
2020-07-27 15:00:43 -0500

    Add task 1

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env ptyhon3
+""" Correlation """
+import numpy as np
+
+
+def correlation(C):
+    """ calculates a correlation matrix.
+        Args:
+            C: (numpy.ndarray) containing a covariance matrix.
+        Returns:
+            (numpy.ndarray) containing the correlation matrix.
+    """
+    if type(C) != np.ndarray:
+        raise TypeError('C must be a numpy.ndarray')
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError('C must be a 2D square matrix')
+    var = np.diag(np.diag(C) ** (-1/2))
+    corr = var @ C @ var
+    return corr
