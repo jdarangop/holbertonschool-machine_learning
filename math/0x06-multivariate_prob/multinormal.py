affa6534d54f8bb39bd155f9cb7f282a23527b0e
jdarangop <juandiegoap33@gmail.com>
2020-07-27 18:44:05 -0500

    Add task 2

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" MultiNormal """
+import numpy as np
+
+
+class MultiNormal(object):
+    """ MultiNormal class """
+
+    def __init__(self, data):
+        """ Initializer """
+        if type(data) != np.ndarray or len(data.shape) != 2:
+            raise TypeError('data must be a 2D numpy.ndarray')
+        d, n = data.shape
+        if n < 2:
+            raise ValueError('data must contain multiple data points')
+        X = data.T
+        mean = np.mean(X, axis=0, keepdims=True)
+        cov = np.matmul((X - mean).T, (X - mean)) / (n - 1)
+        self.mean = mean
+        self.cov = cov
