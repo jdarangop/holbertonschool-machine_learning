070568fa92b396915d10a127c44149803b21ec37
jdarangop <juandiegoap33@gmail.com>
2020-07-28 10:36:52 -0500

    Fix task 3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -31,7 +31,7 @@ class MultiNormal(object):
             raise TypeError('x must be a numpy.ndarray')
         if len(x.shape) != 2 or x.shape[1] != 1:
             raise ValueError('x must have the shape ({d}, 1)'
-                             .format(self.data[0]))
+                             .format(self.cov.shape[0]))
         n = x.shape[0]
         den = np.sqrt(((2 * np.pi) ** n) * np.linalg.det(self.cov))
         cov_inv = np.linalg.inv(self.cov)
