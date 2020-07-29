57074bd49916c55276645bc9577341c568fc1c38
jdarangop <juandiegoap33@gmail.com>
2020-07-29 12:04:15 -0500

    Fix task 0

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -11,9 +11,9 @@ def pca(X, var=0.95):
              the weights matrix, W, that maintains
              var fraction of Xs original variance.
     """
-    U, P, V = np.linalg.svd(X.T, full_matrices=False)
+    U, P, V = np.linalg.svd(X, full_matrices=False)
     threshold = np.sum(P) * var
     index = np.argmax(np.cumsum(P) > threshold)
     # result = np.matmul(U[:, :index], np.diag(P[:index]))
     # result = np.matmul(np.linalg.inv(X), tmp)
-    return U[:, :index + 1]
+    return V.T[:, :index + 1]
