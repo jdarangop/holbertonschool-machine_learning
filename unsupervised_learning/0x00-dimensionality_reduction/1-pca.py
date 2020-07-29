4f2302203900efc9fa71ca7efc2a8a9d7dc0b867
jdarangop <juandiegoap33@gmail.com>
2020-07-29 13:21:13 -0500

    Add task 1

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" PCA """
+import numpy as np
+
+
+def pca(X, ndim):
+    """ performs PCA on a dataset.
+        Args:
+            X: (numpy.ndarray) dataset.
+            ndim: (int) the new dimensionality of the
+                  transformed X.
+        Returns:
+             (numpy.ndarray) containing the transformed version of X.
+    """
+    X_m = X - np.mean(X, axis=0)
+    U, P, V = np.linalg.svd(X_m, full_matrices=False)
+    # threshold = np.sum(P) * var
+    # index = np.argmax(np.cumsum(P) > threshold)
+    result = np.matmul(U[:, :ndim], np.diag(P[:ndim]))
+    return result
