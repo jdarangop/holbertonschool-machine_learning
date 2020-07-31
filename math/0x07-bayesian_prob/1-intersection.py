40cf6ab2922587c3262e1b8c4105665ae76ec47b
jdarangop <juandiegoap33@gmail.com>
2020-07-31 12:52:50 -0500

    Add task 1

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -4,16 +4,15 @@ import numpy as np
 import math
 
 
-def likelihood(x, n, P):
-    """ calculates the likelihood of obtaining
-        this data given various hypothetical
-        probabilities of developing severe
-        side effects.
+def intersection(x, n, P, Pr):
+    """ calculates the intersection of obtaining this
+        data with the various hypothetical probabilities.
         Args:
             x: (int) number of patients.
             n: (int) total number of patients observed.
-            P: (numpy.ndarray)  containing the various
-               hypothetical probabilities
+            P: (numpy.ndarray) containing the various
+               hypothetical probabilities.
+            Pr: (numpy.ndarray) containing the prior beliefs of P.
         Returns:
             (numpy.ndarray) containing the likelihood
                             of obtaining the data.
@@ -27,11 +26,15 @@ def likelihood(x, n, P):
         raise ValueError('x cannot be greater than n')
     if type(P) != np.ndarray:
         raise TypeError('P must be a 1D numpy.ndarray')
+    if type(Pr) != np.ndarray or P.shape != Pr.shape:
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
+    for i in P:
+        if i < 0 or i > 1:
+            raise ValueError('All values in P must be in the range [0, 1]')
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
     result = np.zeros(P.shape)
     comb = math.factorial(n) / (math.factorial(x) * math.factorial(n - x))
     for i in range(P.shape[0]):
-        if P[i] < 0 or P[i] > 1:
-            raise ValueError('All values in P must be in the range [0, 1]')
-        else:
-            result[i] = comb * (P[i] ** x) * ((1 - P[i]) ** (n - x))
+        result[i] = comb * (P[i] ** x) * ((1 - P[i]) ** (n - x)) * Pr[i]
     return result
