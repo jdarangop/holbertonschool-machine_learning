f0fc3c60ca412812c3819068fea90e98b5f476d5
jdarangop <juandiegoap33@gmail.com>
2020-08-06 23:07:15 -0500

    Add task 5 and 6

---
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" PDF """
+import numpy as np
+
+
+def pdf(X, m, S):
+    """ calculates the probability density
+        function of a Gaussian distribution.
+        Args:
+            X: (numpy.ndarray) containing the data points
+                               whose PDF should be evaluated.
+            m: (numpy.ndarray) containing the mean of the distribution.
+            S: (numpy.ndarray) containing the covariance
+                               of the distribution.
+        Returns:
+            (numpy.ndarray) containing the PDF values for each data point.
+    """
+    if type(X) != np.ndarray or len(X.shape) != 2:
+        return None
+    if type(m) != np.ndarray or len(m.shape) != 1:
+        return None
+    if type(S) != np.ndarray or len(S.shape) != 2:
+        return None
+    n, d = X.shape
+    den = np.sqrt(((2 * np.pi) ** d) * np.linalg.det(S))
+    cov_inv = np.linalg.inv(S)
+    expo = (-0.5 * np.sum(np.matmul(cov_inv, (X.T - m[:, np.newaxis])) *
+                          (X.T - m[:, np.newaxis]), axis=0))
+    result = (1 / den) * np.exp(expo)
+    np.where(result < 1e-300, 1e-300, result)
+    return result
---
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Expectation """
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """ calculates the expectation step
+        in the EM algorithm for a GMM.
+        Args:
+            X: (numpy.ndarray) containing the data set.
+            pi: (numpy.ndarray) containing the priors for
+                                each cluster.
+            m: (numpy.ndarray) containing the centroid means
+                               for each cluster.
+            S: (numpy.ndarray) containing the covariance
+                               matrices for each cluster.
+        Returns:
+            (numpy.ndarray) containing the posterior probabilities
+                            for each data point in each cluster.
+    """
+    if type(X) != np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(pi) != np.ndarray or len(pi.shape) != 1:
+        return None, None
+    if type(m) != np.ndarray or len(m.shape) != 2:
+        return None, None
+    if type(S) != np.ndarray or len(S.shape) != 3:
+        return None, None
+    n, d = X.shape
+    k = pi.shape[0]
+
+    pos = np.zeros((k, n))
+    for i in range(k):
+        pos[i] = pi[i] * pdf(X, m[i], S[i])
+
+    g = pos / np.sum(pos, axis=0)
+    log = np.log(np.sum(pos, axis=0))
+
+    return g, log.sum()
