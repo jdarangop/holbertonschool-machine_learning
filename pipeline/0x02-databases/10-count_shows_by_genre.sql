46bf7e181d08a6245973c6e5f69618acf65d4cc7
jdarangop <juandiegoap33@gmail.com>
2020-11-03 00:10:23 -0500

    Fix task 8, 9 and 10

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- Display number of shows by each genre
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre ORDER BY number_of_shows DESC, genre WHERE number_of_shows != 0;
+GROUP BY genre ORDER BY number_of_shows DESC, genre;
--- pipeline/0x02-databases/8-genre_id_by_show.sql
+++ pipeline/0x02-databases/8-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Display show that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
